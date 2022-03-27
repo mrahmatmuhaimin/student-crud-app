3ba67c2da6667fffe736eed46f26aa298bd9ba17
Muhaimin <muhaiminrahmat@gmail.com>
2022-03-27 20:47:41 +0700

    Student Entity

--- src/main/java/id/ac/uin/student/entity/Student.java
+++ src/main/java/id/ac/uin/student/entity/Student.java
@@ -1,12 +1,15 @@
 package id.ac.uin.student.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
-
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "student")
 public class Student {
@@ -14,20 +17,20 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
-    @Column(name = "first_name")
+    @NotEmpty(message = "First name is required")
+    @Column(nullable = false, name = "first_Name")
     private String firstName;
 
-    @NotEmpty
-    @Column(name = "last_name")
+    @NotEmpty(message = "Last name is required")
+    @Column(nullable = false, name = "last_name")
     private String lastName;
 
-    @NotEmpty
-    @Column(name = "email", unique = true)
+    @NotEmpty(message = "email is required")
+    @Column(nullable = false, name = "email", unique = true)
     private String email;
 
-    @NotEmpty
-    @Column(name = "phone_number", unique = true)
+    @NotEmpty(message = "Phone number is required")
+    @Column(nullable = false, unique = true)
     private String phoneNumber;
 
     @NotEmpty(message = "Jurusan is required")
